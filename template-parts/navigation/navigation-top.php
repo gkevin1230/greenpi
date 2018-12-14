12a24cbdbfa7288f0cdfb724ee9634ab40d5e3bf
gkevin1230 <geiger1230@gmail.com>
2018-12-14 16:48:29 +0100

    Initialisation de Git

---
+++ template-parts/navigation/navigation-top.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Displays top navigation
+ *
+ * @package WordPress
+ * @subpackage greenpi
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'greenpi' ); ?>">
+	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
+		<?php
+		echo greenpi_get_svg( array( 'icon' => 'bars' ) );
+		echo greenpi_get_svg( array( 'icon' => 'close' ) );
+		_e( 'Menu', 'greenpi' );
+		?>
+	</button>
+
+	<?php wp_nav_menu( array(
+		'theme_location' => 'top',
+		'menu_id'        => 'top-menu',
+	) ); ?>
+
+	<?php if ( ( greenpi_is_frontpage() || ( is_home() && is_front_page() ) ) && has_custom_header() ) : ?>
+		<a href="#content" class="menu-scroll-down"><?php echo greenpi_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'greenpi' ); ?></span></a>
+	<?php endif; ?>
+</nav><!-- #site-navigation -->
